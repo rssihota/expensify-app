2fa0f0138e81e5ca27459472b8f2be63bde6e793
Ranbir Sihota <ranbirsihota@gmail.com>
2018-07-04 20:49:12 -0400

    array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -20,14 +20,25 @@
 //     console.log(`It's ${temperature} in ${city}`);    
 // }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const { name : publisherName = 'Self-Published' } = book.publisher;
+// const { name : publisherName = 'Self-Published' } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+// Array destructuring
+
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const [itemName, , mediumPrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
