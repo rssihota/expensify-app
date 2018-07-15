22b570d7eeed5b330bf8576bf5510449aa7a9799
Ranbir Sihota <ranbirsihota@gmail.com>
2018-07-15 17:19:44 -0400

    Finishing up react section

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({dispatch, id, description, amount, createdAt }) => (
+const ExpenseListItem = ({id, description, amount, createdAt }) => (
     <div>
-      <h3>{description}</h3>
+      <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
       <p>{amount} - {createdAt}</p>
-      <button onClick={(e) => {dispatch(removeExpense({ id }));
-      }}>Remove</button>
     </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
